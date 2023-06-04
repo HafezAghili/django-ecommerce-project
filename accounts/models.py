bee5c497a48a78c4aec81b348489621c748f7511
Amir Hossein Hafez Aghili <amirhosseinhafezaghili@gmail.com>
2023-06-04 20:34:57 +0430

    before migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from pages.models import City
 from django.contrib.auth.models import AbstractUser 
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+    phone_regex  = RegexValidator(regex=r'^\+?98?\d{9,15}$', message="Phone number must be entered in the format: '+989123456789'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True , default="+989123456789") # Validators should be a list 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.urls import reverse
+from datetime import datetime , timedelta
+
+from accounts.models import CustomUser
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50 , blank=False , null=False)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.IntegerField(default=30)
+    img = models.ImageField()
+    quality = models.CharField(max_length= 10 , choices=[('high quality','high quality') ,
+                                                         ('medium quality','medium quality') ,
+                                                         ('low quality','low quality')])
+    supplier = models.ManyToManyField('Supplier')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    created_at   = models.DateTimeField(auto_now_add=True) 
+    expiration_at = models.DateField(default=datetime.now()+timedelta(days=730))
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[str(self.id)])
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class City(models.Model):
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.city
+    
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=50,blank=False,null=False,choices=[('AbuAuf','AbuAuf'),
+                                                                          ('Abgineh','Abgineh'),
+                                                                          ('Anned','Anned'),
+                                                                          ('Borroje','Borroje'),
+                                                                          ('Diestro','Diestro')])
+    contactInfo = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.name
+
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.ForeignKey('City' , on_delete=models.CASCADE)
+    address = models.CharField(max_length=50) 
+    phone = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.name+' '+self.city
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey('CustomUser' , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart' , on_delete=models.CASCADE)
+    product = models.ForeignKey('Product' , on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50 , default='Submitted' ,choices=[('Submitted','Submitted'),
+                                                                            ('Ready to send','Ready to send'),
+                                                                            ('Sent','Sent')])
+
+    def __str__(self):
+        return self.cart+' '+self.product
+    
+
+class Order(models.Model):
+    user = models.ForeignKey('CustomUser' , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order' , on_delete=models.CASCADE)
+    product = models.ForeignKey('Product' , on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50 , default='Submitted' ,choices=[('Submitted','Submitted'),
+                                                                            ('Ready to send','Ready to send'),
+                                                                            ('Sent','Sent')])
+
+    def __str__(self):
+        return self.order+' '+self.product
