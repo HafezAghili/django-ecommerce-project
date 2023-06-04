a29eb94ae73eec4d70309e5a3dd830bdcc1a7e1a
Amir Hossein Hafez Aghili <amirhosseinhafezaghili@gmail.com>
2023-06-04 17:55:59 +0430

    add account settings

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from pages.models import City
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'pages',
 ]
 
@@ -123,3 +124,5 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
