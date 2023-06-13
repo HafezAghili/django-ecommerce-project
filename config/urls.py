02c3c622d2d2968ec2c2342c6957ab2cb8a87d94
Amir Hossein Hafez Aghili <amirhosseinhafezaghili@gmail.com>
2023-06-13 09:45:12 +0430

    added login and register (not working)

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
