d1befa0c8650a63ca44e6f527a63b297a18c8f84
Hafez <amirhosseinhafezaghili@gmail.com>
2023-06-26 10:59:07 +0330

    change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignUpView , LogoutView
+from .views import SignUpView , LogoutView , ChangePassView
 from . import views
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_change/', ChangePassView.as_view(), name='password_change'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import logout
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.contrib.auth.views import PasswordChangeView
 
 # Create your views here.
 
@@ -16,6 +17,10 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm 
     success_url = reverse_lazy('login') 
     template_name = 'registration/signup.html'
+    
+class ChangePassView(PasswordChangeView):
+    template_name = 'registration/password_change_form.html'
+    success_url = reverse_lazy('login') 
 
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
