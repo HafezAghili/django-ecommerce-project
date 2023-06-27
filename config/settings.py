24afe543dbd4566c4500278b7990c166eb261c42
Hafez <amirhosseinhafezaghili@gmail.com>
2023-06-27 17:06:01 +0330

    setting.py for deploy

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6supmogqna2bbr-+1hr&zznn#@xgl3sh*)(tn1e3e-xozv$$*w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
